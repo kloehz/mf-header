6b6776325431eb3f7b4a358a10a7d4805bfe84b7
Guido Cotelesso <guido.cotelesso@fpayapp.com>
2021-07-13 15:38:15 -0300

    test HashRouter

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,23 +1,27 @@
-import { Link } from "@reach/router"
+import { Route, HashRouter, Link } from 'react-router-dom';
 
-export default function App() {
+export default function Root(props) {
   return (
-    <>
-      <Link path='user'>User</Link>
-      <Link path='body'>Home</Link>
-    </>
+    <HashRouter>
+      <App />
+    </HashRouter>
   );
 }
 
+const App = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      flexDirection: 'row',
+    }}>
+      <Link to='user'><h2>user</h2></Link>
+      <Link to='body'><h2 style={{marginLeft: '20px'}}>body</h2></Link>
 
-// const App = () => {
-//   return (
-//     <div style={{
-//       display: 'flex',
-//       flexDirection: 'row',
-//     }}>
-//       <Link to={'user'}><h2>user</h2></Link>
-//       <Link to={'body'}><h2 style={{marginLeft: '20px'}}>body</h2></Link>
-//     </div>
-//   );
-// }
+      <Route exact path="/" component={Body} />
+      <Route path="/user" component={Users} />
+    </div>
+  );
+}
+
+const Body = () => <div><h2>Body</h2></div>
+const Users = () => <div><h2>Users</h2></div>
